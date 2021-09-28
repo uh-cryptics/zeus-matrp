0288fabd3bb0315c913dc5a9992c80954472fe69
rabitz808 <77714922+rabitz808@users.noreply.github.com>
2021-09-28 12:58:39 -1000

    Update Signout.jsx
    Added button to sign out page to login again.

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { NavLink } from 'react-router-dom';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 const Signout = () => {
   Meteor.logout();
   return (
     <Header id={PAGE_IDS.SIGN_OUT} as="h2" textAlign="center">
-      <p>You are signed out.</p>
+      <p>You have been signed out.</p>
+      <Button content='Login' color='blue' as={NavLink} exact to="/signin"/>
     </Header>
   );
 };
