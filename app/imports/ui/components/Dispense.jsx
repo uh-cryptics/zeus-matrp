c82b56f022bc80cfc95d4d049f26892d3ba61f09
sheenatorres <59987784+sheenatorres@users.noreply.github.com>
2021-09-30 14:00:40 -1000

    added lot number dropdown to dispense form

--- app/imports/ui/components/Dispense.jsx
+++ app/imports/ui/components/Dispense.jsx
@@ -6,6 +6,7 @@ import _ from 'lodash';
 const Dispense = ({ set, open, setOpen }) => {
   const uniqueNames = _.uniq(set.map(item => item.name))
     .map((type, index) => ({ key: `name${index}`, text: type, value: type }));
+  const uniqueLot = _.uniq(set.map(item => item.lot)).map((lot, i) => ({ key: `loc${i}`, text: lot, value: lot }));
 
   return (
     <div>
@@ -33,6 +34,15 @@ const Dispense = ({ set, open, setOpen }) => {
             <Form.Field>
               <Form.Group widths="equal">
                 <Form.Field required>
+                  <label>LOT</label>
+                  <Form.Dropdown
+                    search
+                    selection
+                    placeholder="5678EFGH"
+                    options={uniqueLot}
+                  />
+                </Form.Field>
+                <Form.Field required>
                   <label>Medicine</label>
                   <Form.Dropdown
                     search
@@ -41,9 +51,9 @@ const Dispense = ({ set, open, setOpen }) => {
                     options={uniqueNames}
                   />
                 </Form.Field>
-                <Form.Field required>
+                <Form.Field required width="5">
                   <label>Amount</label>
-                  <Input type="number" name="amount" placeholder="Amount" />
+                  <Input type="number" name="amount" placeholder="#" />
                 </Form.Field>
               </Form.Group>
             </Form.Field>
