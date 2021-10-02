850a9e93293bd53730dd57d37f1b4fdf12f6ae49
lukemcd9 <lukemcd9@hawaii.edu>
2021-10-01 15:35:30 -1000

    Add state variables

--- app/imports/ui/components/Dispense.jsx
+++ app/imports/ui/components/Dispense.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form, Modal, Input } from 'semantic-ui-react';
 import _ from 'lodash';
@@ -7,6 +7,16 @@ const Dispense = ({ set, open, setOpen }) => {
   const uniqueNames = _.uniq(set.map(item => item.name))
     .map((type, index) => ({ key: `name${index}`, text: type, value: type }));
   const uniqueLot = _.uniq(set.map(item => item.lot)).map((lot, i) => ({ key: `loc${i}`, text: lot, value: lot }));
+
+  const [patientNumber, setPatientNumber] = useState('');
+  const [clinicLocation, setClinicLocation] = useState('');
+  const [lotNumber, setLotNumber] = useState('');
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const submit = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
@@ -23,12 +33,12 @@ const Dispense = ({ set, open, setOpen }) => {
               <Form.Field required>
                 <label>Patient Medical Number</label>
                 <Form.Field>
-                  <Input placeholder="Medical Number" name="medical-number" />
+                  <Input placeholder="Medical Number" name="medical-number" onChange={(e) => setPatientNumber(e.target.value)} value={patientNumber}/>
                 </Form.Field>
               </Form.Field>
               <Form.Field required>
                 <label>Clinic Location</label>
-                <Input placeholder="Clinic Location" name="clinic-location"/>
+                <Input placeholder="Clinic Location" name="clinic-location" value={clinicLocation} onChange={(e) => setClinicLocation(e.target.value)}/>
               </Form.Field>
             </Form.Group>
             <Form.Field>
@@ -40,20 +50,24 @@ const Dispense = ({ set, open, setOpen }) => {
                     selection
                     placeholder="5678EFGH"
                     options={uniqueLot}
+                    value={lotNumber}
+                    onChange={(e, data) => setLotNumber(data.value)}
                   />
                 </Form.Field>
                 <Form.Field required>
-                  <label>Medicine</label>
+                  <label>Item</label>
                   <Form.Dropdown
                     search
                     selection
-                    placeholder="Select a medicine to dispense..."
-                    options={uniqueNames}
+                    placeholder="Select a medicine or supply to dispense..."
+                    options={uniqueNames
+                    }value={item}
+                    onChange={(e, data) => setItem(data.value)}
                   />
                 </Form.Field>
                 <Form.Field required width="5">
                   <label>Amount</label>
-                  <Input type="number" name="amount" placeholder="#" />
+                  <Input type="number" name="amount" placeholder="#" value={amount} onChange={(e) => setAmount(e.target.value)}/>
                 </Form.Field>
               </Form.Group>
             </Form.Field>
@@ -61,7 +75,7 @@ const Dispense = ({ set, open, setOpen }) => {
         </Modal.Content>
         <Modal.Actions>
           <Button secondary onClick={() => setOpen(false)}>Cancel</Button>
-          <Button primary onClick={() => setOpen(false)}>Dispense</Button>
+          <Button primary onClick={() => submit()}>Dispense</Button>
         </Modal.Actions>
       </Modal>
     </div>
