f5053e576691972b2970096b3533d09ea70a9198
lukemcd9 <lukemcd9@hawaii.edu>
2021-10-02 14:24:16 -1000

    Add functionality to AddMedication and AddSupply

--- app/imports/ui/components/AddMedication.jsx
+++ app/imports/ui/components/AddMedication.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Modal } from 'semantic-ui-react';
+import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import _ from 'lodash';
+import { defineMethod } from '../../api/base/BaseCollection.methods';
+import swal from 'sweetalert';
+import { Medication } from '../../api/medication/MedicationCollection';
 
 const AddMedication = ({ set, open, setOpen }) => {
   const uniqueMedType = _.uniq(_.flatten(set.map(item => item.type))).map((type, index) => ({ key: `medType${index}`, text: type, value: type }));
   const uniqueLocations = _.uniq(set.map(item => item.location)).map((location, i) => ({ key: `loc${i}`, text: location, value: location }));
+
+  const [name, setName] = useState('');
+  const [type, setType] = useState([]);
+  const [location, setLocation] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [expiration, setExpiration] = useState('');
+  const [obtained, setObtained] = useState('Donated');
+  const [lot, setLot] = useState('');
+  const [error, setError] = useState({ has: false, message: '' });
+
+  const submit = () => {
+    if (name && (type.length > 0) && location && _.isNumber(quantity) && expiration && obtained && lot) {
+      const definitionData = { name, type, location, quantity, expiration, obtained, lot };
+      defineMethod.callPromise({ collectionName: Medication.getCollectionName(), definitionData })
+        .catch(e => swal('Error', e.message, 'error'))
+        .then(() => {
+          swal('Success', `Added ${name}`, 'success').then(() => clear());
+        });
+    } else {
+      setError({ has: true, message: 'Please input all required fields' });
+    }
+  };
+
+  const clear = () => {
+    setName('');
+    setType([]);
+    setLocation('');
+    setQuantity('');
+    setExpiration('');
+    setObtained('Donated');
+    setLot('');
+    setError({ has: false, message: '' });
+    setOpen(false);
+  };
+
   return (
     <div>
       <Modal
@@ -14,10 +52,17 @@ const AddMedication = ({ set, open, setOpen }) => {
         open={open}
         size='small'
       >
-        <Modal.Header>Add Medication</Modal.Header>
+        <Modal.Header>
+          <Icon.Group size='large'>
+            <Icon name='pills' />
+            <Icon color='blue' corner='top right' name='add' />
+          </Icon.Group>
+          &nbsp;
+          Add Medication
+        </Modal.Header>
         <Modal.Content>
-          <Form>
-            <Form.Input required name='name' label='Name' placeholder='Aspirin 100mg Tablets' />
+          <Form error={error.has}>
+            <Form.Input required name='name' label='Name' placeholder='Aspirin 100mg Tablets' value={name} onChange={(e) => setName(e.target.value)}/>
             <Form.Dropdown
               required
               multiple
@@ -26,6 +71,8 @@ const AddMedication = ({ set, open, setOpen }) => {
               search
               selection
               options={uniqueMedType}
+              value={type}
+              onChange={(e, data) => setType(data.value)}
             />
             <Form.Group widths='equal'>
               <Form.Dropdown
@@ -36,28 +83,39 @@ const AddMedication = ({ set, open, setOpen }) => {
                 selection
                 label='Location'
                 options={uniqueLocations}
+                value={location}
+                onChange={(e, data) => setLocation(data.value)}
               />
-              <Form.Input required name='quantity' label='Quantity' placeholder='Quantity' />
-              <Form.Input required name='expiration' type='date' label='Expiration' placeholder='MM/DD/YYYY' />
+              <Form.Input required name='quantity' label='Quantity' placeholder='Quantity'
+                          value={quantity}
+                          onChange={(e) => setQuantity(parseInt(e.target.value, 10))}/>
+              <Form.Input required name='expiration' type='date' label='Expiration' placeholder='MM/DD/YYYY'
+                          value={expiration}
+                          onChange={(e) => setExpiration(e.target.value)} />
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Field required name='obtained' label='Obtained' control='select'>
+              <Form.Field required name='obtained' label='Obtained' control='select'
+                          value={obtained}
+                          onChange={(e) => setObtained(e.target.value)}>
                 <option value='Donated'>Donated</option>
                 <option value='Purchased'>Purchased</option>
               </Form.Field>
-              <Form.Input required name='lot' label='LOT' placeholder='1A2B3C4D' />
+              <Form.Input required name='lot' label='LOT' placeholder='1A2B3C4D'
+                          value={lot}
+                          onChange={(e) => setLot(e.target.value)}/>
             </Form.Group>
+            <Message error header='Error' content={error.message}/>
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color='black' onClick={() => setOpen(false)}>
+          <Button color='black' onClick={() => clear()}>
             Cancel
           </Button>
           <Button
             content="Submit"
             labelPosition='right'
             icon='checkmark'
-            onClick={() => setOpen(false)}
+            onClick={() => submit()}
             positive
           />
         </Modal.Actions>
--- app/imports/ui/components/AddSupply.jsx
+++ app/imports/ui/components/AddSupply.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Modal } from 'semantic-ui-react';
+import { Button, Form, Icon, Message, Modal } from 'semantic-ui-react';
 import _ from 'lodash';
+import { defineMethod } from '../../api/base/BaseCollection.methods';
+import swal from 'sweetalert';
+import { Supply } from '../../api/supply/SupplyCollection';
 
 const AddSupply = ({ set, open, setOpen }) => {
   const uniqueLocations = _.uniq(set.map(item => item.location)).map((location, i) => ({ key: `loc${i}`, text: location, value: location }));
+
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [obtained, setObtained] = useState('Donated');
+  const [lot, setLot] = useState('');
+  const [error, setError] = useState({ has: false, message: '' });
+
+  const submit = () => {
+    if (name && location && _.isNumber(quantity) && obtained && lot) {
+      const definitionData = { name, location, quantity, obtained, lot };
+      defineMethod.callPromise({ collectionName: Supply.getCollectionName(), definitionData })
+        .catch(e => swal('Error', e.message, 'error'))
+        .then(() => {
+          swal('Success', `Added ${name}`, 'success').then(() => clear());
+        });
+    } else {
+      setError({ has: true, message: 'Please input all required fields' });
+    }
+  };
+
+  const clear = () => {
+    setName('');
+    setLocation('');
+    setQuantity('');
+    setObtained('Donated');
+    setLot('');
+    setError({ has: false, message: '' });
+    setOpen(false);
+  };
+
   return (
     <div>
       <Modal
@@ -13,10 +47,17 @@ const AddSupply = ({ set, open, setOpen }) => {
         open={open}
         size='small'
       >
-        <Modal.Header>Add Supply</Modal.Header>
+        <Modal.Header>
+          <Icon.Group size='large'>
+            <Icon name='first aid' />
+            <Icon color='blue' corner='top right' name='add' />
+          </Icon.Group>
+          &nbsp;
+          Add Supply
+        </Modal.Header>
         <Modal.Content>
-          <Form>
-            <Form.Input required name='name' label='Name' placeholder='Theraband - Green' />
+          <Form error={error.has}>
+            <Form.Input required name='name' label='Name' placeholder='Theraband - Green' value={name} onChange={(e) => setName(e.target.value)}/>
             <Form.Group widths='equal'>
               <Form.Dropdown
                 required
@@ -26,27 +67,30 @@ const AddSupply = ({ set, open, setOpen }) => {
                 selection
                 label='Location'
                 options={uniqueLocations}
+                value={location}
+                onChange={(e, data) => setLocation(data.value)}
               />
-              <Form.Input required name='quantity' label='Quantity' placeholder='Quantity' />
+              <Form.Input required name='quantity' label='Quantity' placeholder='Quantity' value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value, 10))} />
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Field required name='obtained' label='Obtained' control='select'>
+              <Form.Field required name='obtained' label='Obtained' control='select' value={obtained} onChange={(e) => setObtained(e.target.value)}>
                 <option value='Donated'>Donated</option>
                 <option value='Purchased'>Purchased</option>
               </Form.Field>
-              <Form.Input required name='lot' label='LOT' placeholder='1A2B3C4D' />
+              <Form.Input required name='lot' label='LOT' placeholder='1A2B3C4D' value={lot} onChange={(e) => setLot(e.target.value)}/>
             </Form.Group>
+            <Message error header='Error' content={error.message}/>
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color='black' onClick={() => setOpen(false)}>
+          <Button color='black' onClick={() => clear()}>
             Cancel
           </Button>
           <Button
             content="Submit"
             labelPosition='right'
             icon='checkmark'
-            onClick={() => setOpen(false)}
+            onClick={() => submit()}
             positive
           />
         </Modal.Actions>
