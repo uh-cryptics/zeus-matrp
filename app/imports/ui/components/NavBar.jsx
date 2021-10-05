a0c5aeff77951f4172598d2134059dad0c966f85
rabitz808 <77714922+rabitz808@users.noreply.github.com>
2021-10-05 11:50:28 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -12,7 +12,7 @@ import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 const NavBar = ({ currentUser }) => {
   const menuStyle = { marginBottom: '10px' };
   return (
-    <Menu style={menuStyle} attached="top" borderless inverted>
+    <Menu style={menuStyle} attached="top" borderless inverted color="black">
       <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/list">
         <Header inverted color="black" as='h1'>ZEUS</Header>
       </Menu.Item>
