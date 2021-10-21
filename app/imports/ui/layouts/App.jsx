3279e3647e4395b3216865a79d80debfae79e7bd
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-10-20 23:52:45 -1000

    Solving issues 035 and 029

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -14,6 +14,7 @@ import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
 import ManageDatabase from '../pages/ManageDatabase';
 import { ROLE } from '../../api/role/Role';
+import ListHistory from '../pages/ListHistory';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -31,6 +32,7 @@ class App extends React.Component {
             {/* <ProtectedRoute path="/add" component={AddStuff}/>
             <ProtectedRoute path="/edit/:_id" component={EditStuff}/> */}
             <AdminProtectedRoute path="/manage-database" component={ManageDatabase}/>
+            <AdminProtectedRoute path="/patient-history" component={ListHistory}/>
             <Route component={NotFound}/>
           </Switch>
           <Footer/>
